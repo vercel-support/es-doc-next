afa8cd092a4d9fabe1179b7bf5363954a15ed639
Yegor Gunko <57867800+yegorgunko@users.noreply.github.com>
2021-05-31 14:10:39 +0300

    Добавлен вариант сборки для продукции и для разработки

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,8 +3,12 @@ import type { DefaultThemeOptions } from 'vuepress'
 
 import { description } from '../../package.json'
 
+const isProd = process.env.NODE_ENV === 'production'
+
 export default defineUserConfig<DefaultThemeOptions>({
   lang: 'ru',
   title: 'ES Doc',
   description,
+
+  bundler: isProd ? '@vuepress/webpack' : '@vuepress/vite',
 })
