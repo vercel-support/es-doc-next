dfa3b8d093c6cbcc4bb5e956c56ae0080a99d52d
Yegor Gunko <57867800+yegorgunko@users.noreply.github.com>
2021-06-08 15:50:05 +0300

    добавлены ЦГ сообщества

---
+++ src/.vuepress/components/Tables/Community/Images/Cgs.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { communityCgs } from '@temp/resources'
+
+const cgs = communityCgs
+</script>
+
+<template>
+  <Tables-Base
+    type="img"
+    :data="cgs"
+    codeTemplate="scene %"
+    file="community_cgs_list"
+  />
+</template>
