4a94d59f64450c4ee908ebbdd570bd79263e0c46
Yegor Gunko <57867800+yegorgunko@users.noreply.github.com>
2021-06-09 14:14:55 +0300

    перенесена часть контента

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -22,7 +22,87 @@ export default defineUserConfig<DefaultThemeOptions>({
   bundler: isProd ? '@vuepress/webpack' : '@vuepress/vite',
 
   themeConfig: {
-    navbar: ['/guide/'],
+    navbar: [
+      {
+        text: 'Руководство',
+        link: '/guide/',
+      },
+      {
+        text: 'Таблицы ресурсов',
+        children: [
+          {
+            text: 'Ресурсы игры',
+            link: '/resources/default/',
+          },
+          {
+            text: 'Ресурсы сообщества',
+            link: '/resources/community/',
+          },
+        ],
+      },
+      {
+        text: 'Разное',
+        link: '/misc/',
+      },
+    ],
+    sidebar: {
+      '/guide/': [
+        '',
+        'advanced.md',
+        'videos.md',
+        'errors.md',
+        'code-examples.md',
+      ],
+      '/resources/': [
+        {
+          text: 'Ресурсы игры',
+          // link: '/resources/default/',
+          isGroup: true,
+          children: [
+            'default/characters.md',
+            {
+              text: 'Изображения',
+              isGroup: true,
+              children: ['default/images/bgs.md', 'default/images/cgs.md'],
+            },
+            {
+              text: 'Звуки',
+              isGroup: true,
+              children: [
+                'default/sounds/ambiences.md',
+                'default/sounds/music.md',
+                'default/sounds/sfx.md',
+              ],
+            },
+          ],
+        },
+        {
+          text: 'Ресурсы сообщества',
+          // link: '/resources/community/',
+          isGroup: true,
+          children: [
+            {
+              text: 'Изображения',
+              isGroup: true,
+              children: ['community/images/bgs.md', 'community/images/cgs.md'],
+            },
+            {
+              text: 'Звуки',
+              isGroup: true,
+              children: ['community/sounds/music.md'],
+            },
+          ],
+        },
+      ],
+      '/misc/': [
+        {
+          text: 'Разное',
+          link: '/misc/',
+          children: ['news-sources.md', 'artists.md', 'literature.md'],
+        },
+      ],
+    },
+    sidebarDepth: 3,
 
     tip: 'Совет',
     warning: 'Внимание!',
